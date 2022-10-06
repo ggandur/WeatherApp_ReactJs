2b552edc478ad03670b04b3f6d271b3a0e6b6f96
Madson Santana <madson.santana96@gmail.com>
2022-10-06 15:35:52 +0000

    Exemplos do getByLocation com fetch e axios, com async await E promises

--- src/functions/functions.tsx
+++ src/functions/functions.tsx
@@ -1,13 +1,72 @@
-import axios from "axios"
+import axios, { AxiosResponse } from "axios"
 
-export const getByLocation = async (setLoading, finalUrl, setData) => {
+type SetLoadingType = (stateLoading: boolean) => void
+
+export const getByLocationPromised = (setLoading: SetLoadingType, finalUrl, setData) => {
+    setLoading(true)
+
+    const promiseResult = axios.get(finalUrl)
+
+    const responseCallback = (response: AxiosResponse) => {
+        setData(response.data)
+        console.log('Resposta', response.headers)
+    }
+    const errorCallback = (err) => { console.log(err) }
+    const onFinally = () => setLoading(false)
+
+    promiseResult
+        .then(responseCallback)
+        .catch(errorCallback)
+        .finally(onFinally)
+
+}
+
+export const getByLocationAsync = async (setLoading, finalUrl, setData) => {
     setLoading(true)
 
     try {
-        await axios.get(finalUrl).then((response) => {
-            setData(response.data)
-            console.log('Resposta', response.data)
-        })
+        const result = await axios.get(finalUrl);
+        setData(result.data)
+        console.log('Resposta', result.data)
+    } catch (e) {
+        console.log(e)
+    }
+
+    setLoading(false)
+}
+
+// --------------------------------------------------
+
+
+export const getByLocationPromisedFetch = (setLoading: SetLoadingType, finalUrl, setData) => {
+    setLoading(true)
+
+    const promiseResult = fetch(finalUrl)
+
+    const responseCallback = (response: Response) => {
+        const jsonResult = response.json()
+
+        jsonResult.then(setData)
+        jsonResult.catch()
+    }
+    const errorCallback = (err) => { console.log(err) }
+
+    promiseResult.then(responseCallback)
+    promiseResult.catch(errorCallback)
+
+    setLoading(false)
+}
+
+
+export const getByLocationAsyncFetch = async (setLoading, finalUrl, setData) => {
+    setLoading(true)
+
+    try {
+        const result = await fetch(finalUrl);
+        const jsonResult = await result.json()
+
+        setData(jsonResult)
+        console.log('Resposta', result.headers)
     } catch (e) {
         console.log(e)
     }
